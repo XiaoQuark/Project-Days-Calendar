2ccdb065eccac49311ac1a8b00cfbcce7a145a59
Farah-Stu <selma.salam45@gmail.com>
2026-06-15 18:47:40 +0100

    setting up js file

--- web.mjs
+++ web.mjs
@@ -4,9 +4,12 @@
 // You can't open the index.html file using a file:// URL.
 
 import { getGreeting } from "./common.mjs";
-import daysData from "../data/days.json" with { type: "json" };
+import daysData from "./days.json" with { type: "json" };
 
-window.onload = function () {
-  document.querySelector("body").innerText =
-    `${getGreeting()} - there are ${daysData.length} known days`;
-};
+
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+
+
