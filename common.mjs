e6aac6fd334cbf8b8b490439b587897b65863d8d
Farah-Stu <selma.salam45@gmail.com>
2026-06-16 01:08:48 +0100

    populating the month/year dropdown

--- common.mjs
+++ common.mjs
@@ -1,5 +1,24 @@
-// This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
+export const monthsArr = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-export function getGreeting() {
-  return "Hello";
-}
+export const weeksDayArr = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
--- web.mjs
+++ web.mjs
@@ -3,8 +3,51 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getGreeting } from "./common.mjs";
+import { monthsArr, weeksDayArr } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
+// Track current month/year
+
+const currentDate = new Date();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
 
 
+//DOM references
+
+const calendarDiv = document.getElementById("calendar");
+const monthHeader = document.getElementById("current-month");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const monthDropdown = document.getElementById("month-select");
+const yearDropdown = document.getElementById("year-select");
+
+// Populate year/month dropdown
+
+function populateDropdowns(){
+
+//month
+monthDropdown.innerHTML = "";
+monthsArr.forEach((monthName, index)=>{
+const option = document.createElement("option");
+option.textContent = monthName;
+option.value = index;
+monthDropdown.appendChild(option);
+});
+
+ monthDropdown.value = currentMonth;
+
+}
+// year
+
+yearDropdown.innerHTML = "";
+for(let y= 1900; y<= 2100; y++){
+    const option = document.createElement("option");
+    option.textContent = y;
+    option.value = y;
+    yearDropdown.appendChild(option);
+};
+yearDropdown.value = currentYear;
+
+
+populateDropdowns();
