5588226b4e0b246dfa5d1f1932d67e05e55ce620
Farah-Stu <selma.salam45@gmail.com>
2026-06-18 21:15:21 +0100

    Fix incorrect import path in test file

--- test/common.test.mjs
+++ test/common.test.mjs
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { getEventDate } from "./common.mjs";
+import { getEventDate } from "../src/common.mjs";
 
 describe("getEventDate", () => {
   const event_ = {
